21aab14d73861cddfe0935dab82ab699af36200c
Simen Monstad <simen@dhcp-128-39-29-209.wlan.hio.no>
2012-02-13 14:02:19 +0100

    Added rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mesg::Application.config.secret_token = '27c8965ce37c0a8053da497fa7455dadba4b1922d95c8c40689b737e0e0d727817913e4d0f61df980921958844491dbb3a7baf6ce56279fb88974f7387113ab9'
